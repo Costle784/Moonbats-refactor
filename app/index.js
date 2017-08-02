204bdc962c6c5c7b6bb19499ec26f1760843e612
Curtis Ostle <curtisostle@gmail.com>
2017-08-02 10:04:23 -0400

    set up package.json, .gitignore, node modules, initial component

---
+++ app/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+require('./components/App'); 
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
