2c6dbf72dfcfa6071bade35718f4cc785a14ce96
Curtis Ostle <curtisostle@gmail.com>
2017-08-08 10:25:12 -0400

    starting presentational components

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ var React = require('react');
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         Hello World
       </div>
     )
