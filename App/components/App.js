4d3b0eb26360012565f67720c58fdd487b8f2c22
Curtis Ostle <curtisostle@gmail.com>
2017-08-09 10:28:02 -0400

    this working?

--- App/components/App.js
+++ App/components/App.js
@@ -3,17 +3,32 @@ var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
+var Redirect = ReactRouter.Redirect;
 var Header = require('./Header');
 var Home = require('./Home');
 
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+  
+  }
+  addSchedule(e) {
+    console.log(e.target);
+    console.log('hello');
+  }
+
   render() {
     return (
       <Router>
         <div className='container'>
           <Header />
-          <Route exact path='/Moonbats' component={Home} />
+          <Switch>
+            <Route exact path='/Moonbats' component={() => (<Home handleChange={(e) => this.addSchedule(e)} />)}/>
+            <Redirect from='/' to='/Moonbats' />
+            <Route exact path='/teams/:id/games' />
+          </Switch>
         </div>
       </Router>
     )
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,28 +1,33 @@
 var React = require('react');
 
-function TeamPicker() {
-  let teams = ['Washington Nationals', 'Philadelphia Phillies', 'Atlanta Braves', 'New York Mets', 'Miami Marlins']
-
+function TeamPicker(props) {
+  let baseballTeams = ['Washington Nationals', 'Philadelphia Phillies', 'Atlanta Braves', 'New York Mets', 'Miami Marlins']
+  let teams = baseballTeams.map((team, index) => {
+    return (
+      <option
+        key={team}
+        value={index + 1}>
+        {team}
+      </option>
+    )
+  })
   return (
-      <select>
-        {teams.map((team, index) => {
-          return (
-            <option
-              key={team}
-              value={index + 1}>
-              {team}
-            </option>
-          )
-        })}
+      <select onChange={(e) => props.onChange(e)}>
+        {teams}
       </select>
   )
 }
 
 
 
-function Home() {
+function Home(props) {
+
   return (
-    <TeamPicker />
+    <div className='home-container'>
+      <TeamPicker onChange={(e) => props.handleChange(e)}  />
+      <p><img src='https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif'></img></p>
+      <img className='bats' src='http://www.pngmart.com/files/1/Baseball-Bat-Transparent-PNG.png'></img>
+    </div>
   )
 }
 module.exports = Home;
