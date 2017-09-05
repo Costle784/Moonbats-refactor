9e8d86ae268ff920815a962f8e3ea5b1974f7c2d
Curtis Ostle <curtisostle@gmail.com>
2017-09-05 17:52:00 -0400

    webpack dev-server problems

--- react/App/components/Home.js
+++ react/App/components/Home.js
@@ -1,32 +1,13 @@
 var React = require('react');
 
-function TeamPicker(props) {
-  let baseballTeams = ['Washington Nationals', 'Philadelphia Phillies', 'Atlanta Braves', 'New York Mets', 'Miami Marlins']
-  let teams = baseballTeams.map((team, index) => {
-    return (
-      <option
-        key={team}
-        value={index + 1}>
-        {team}
-      </option>
-    )
-  })
-  return (
-      <select onChange={(e) => props.onChange(e)}>
-        {teams}
-      </select>
-  )
-}
 
 
-
-function Home(props) {
-
+function Home() {
   return (
     <div className='home-container'>
-      <TeamPicker onChange={(e) => props.handleChange(e)}  />
-      <p><img src='https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif'></img></p>
-      <img className='bats' src='http://www.pngmart.com/files/1/Baseball-Bat-Transparent-PNG.png'></img>
+    
+      <p><img src='https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif' alt='spinning moon'></img></p>
+      <img src='https://images.fineartamerica.com/images-medium-large/baseball-moon-bill-cannon.jpg' alt='baseball'></img>
     </div>
   )
 }
