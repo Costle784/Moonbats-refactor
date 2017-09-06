3108f9d5f31bffd86039856c85f5c38fbf4eb014
Curtis Ostle <curtisostle@gmail.com>
2017-09-06 11:52:35 -0400

    added button on home page

--- react/App/components/Home.js
+++ react/App/components/Home.js
@@ -1,15 +1,17 @@
 var React = require('react');
+var Link = require('react-router-dom').Link;
 
 
 
 function Home() {
   return (
     <div className='home-container'>
-    
-
-      <h1>Click to begin</h1>
-
+      <Link className='button-container' to='./teams'>
+        Click to begin
+        <img src='http://www.pngmart.com/files/1/Baseball-Bat-Transparent-PNG.png' className='bats' alt='baeball bats link'></img>
+      </Link>
     </div>
+
   )
 }
 module.exports = Home;
