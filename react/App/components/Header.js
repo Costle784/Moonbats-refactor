8e6aee531d9654d7022a90fa775f3a68e5990fd1
Curtis Ostle <curtisostle@gmail.com>
2017-09-05 21:11:53 -0400

    made moonball

--- react/App/components/Header.js
+++ react/App/components/Header.js
@@ -5,9 +5,6 @@ function Header() {
   return (
     <div className='header'>
       <h1 className='title'>Moonbats</h1>
-      <nav>
-        <Link to="/Moonbats" className='home'>Home</Link>
-      </nav>
     </div>
   )
 }
--- react/App/components/Home.js
+++ react/App/components/Home.js
@@ -5,9 +5,13 @@ var React = require('react');
 function Home() {
   return (
     <div className='home-container'>
-    
-      <p><img src='https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif' alt='spinning moon'></img></p>
-      <img src='https://images.fineartamerica.com/images-medium-large/baseball-moon-bill-cannon.jpg' alt='baseball'></img>
+
+      <div className='image-container'>
+        <img src='https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif' alt='spinning moon' className='moonball'></img>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/f/f3/Balle_de_baseball.png' className='stitches'></img>
+      </div>
+      <h1>Click to begin</h1>
+
     </div>
   )
 }
