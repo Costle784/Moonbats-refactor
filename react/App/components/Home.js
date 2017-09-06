61d156fbe80891ecef96072f341ed1739aeac442
Curtis Ostle <curtisostle@gmail.com>
2017-09-06 11:08:46 -0400

    homepage css;
    Former-commit-id: 47a13e8153af7999b4d358af13951138d69cd95a

--- react/App/components/Home.js
+++ react/App/components/Home.js
@@ -5,11 +5,8 @@ var React = require('react');
 function Home() {
   return (
     <div className='home-container'>
+    
 
-      <div className='image-container'>
-        <img src='https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif' alt='spinning moon' className='moonball'></img>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/f/f3/Balle_de_baseball.png' className='stitches'></img>
-      </div>
       <h1>Click to begin</h1>
 
     </div>
