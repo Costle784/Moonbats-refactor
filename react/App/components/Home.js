349a9e1a57e99e8f28952531cbec10dd6e09ae52
Curtis Ostle <curtisostle@gmail.com>
2017-09-08 09:56:22 -0400

    configuring teams component

--- react/App/components/Home.js
+++ react/App/components/Home.js
@@ -6,9 +6,9 @@ var Link = require('react-router-dom').Link;
 function Home() {
   return (
     <div className='home-container'>
-      <Link className='button-container' to='./teams'>
+      <Link className='bat-button' to='./teams'>
         <img src='http://www.pngmart.com/files/1/Baseball-Bat-Transparent-PNG.png' className='bats' alt='baeball bats link'></img>
-        <p>Click to begin</p>
+        <p className='begin'>Click to begin</p>
       </Link>
 
     </div>
