93748d1b1e5f932981e8a1878c7e5bc63341bc2f
Curtis Ostle <curtisostle@gmail.com>
2017-09-08 09:56:22 -0400

    configuring teams component
    Former-commit-id: 349a9e1a57e99e8f28952531cbec10dd6e09ae52

--- react/app/components/Teams.js
+++ react/app/components/Teams.js
@@ -1,10 +1,44 @@
 var React = require('react');
+var NavLink = require('react-router-dom').NavLink;
+
+
+function SelectLeague(props) {
+  let leagues = ['All','NL','AL'];
+
+  return (
+    <ul className='leagues'>
+      {leagues.map((league) => {
+        return (
+          <li
+            style={league === props.league ? {color:'#FF0000'} : null}
+            onClick={props.onSelect.bind(null,league)}
+            key={league}>
+            {league}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+
 
 
 class Teams extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      league: 'All',
+      teams: null
+    }
+  }
+
   render() {
     return (
-      <div>Teams</div>
+      <div>
+        <SelectLeague />
+        <div>Teams</div>
+      </div>
     )
   }
 }
