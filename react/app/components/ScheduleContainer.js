949309a387816f6f4feb1d64b74387ab15877f76
Curtis Ostle <curtisostle@gmail.com>
2017-09-21 22:18:46 -0400

    building schedule container

--- react/app/components/ScheduleContainer.js
+++ react/app/components/ScheduleContainer.js
@@ -1,7 +1,35 @@
 var React = require('react');
+var api = require('../utils/api');
 
 class ScheduleContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      team:{},
+      games:[],
+      logo:'',
+    }
+  }
+
+  componentDidMount() {
+    let teamSymbol = this.props.match.params.symbol
+
+    let bteam;
+
+    api.getTeam(teamSymbol)
+      .then((team) => {
+        api.getGames(team)
+        this.setState({
+          logo: team[0].logo
+        })
+      })
+
+
+  }
+
   render() {
+
+
     return (
       <div>Schedule</div>
     )
