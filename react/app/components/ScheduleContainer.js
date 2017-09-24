6c2b84046f78b56c9fd6a6ab7f037bad7521ecbe
Curtis Ostle <curtisostle@gmail.com>
2017-09-24 17:18:40 -0400

    got opp names on game items

--- react/app/components/ScheduleContainer.js
+++ react/app/components/ScheduleContainer.js
@@ -2,23 +2,22 @@ var React = require('react');
 var api = require('../utils/api');
 var Link = require('react-router-dom').Link;
 
-
-function Logo(props) {
-  return(
-    <img
-      src={props.team.logo}
-      alt={`${props.team} logo`}
-      className='logo'/>
-  )
-}
-
-
 function displayDate(date) {
   let newDate = [];
   let d = date.split('');
 
   newDate.push(d[5], d[6], d[7], d[8],d[9],'-',d[0], d[1], d[2], d[3]);
   return newDate.join('')
+}
+
+function Logo(props) {
+  return(
+    <img
+      src={props.team.logo}
+      alt={`${props.team.name} logo`}
+      className='logo'
+    />
+  )
 }
 
 class Game extends React.Component{
@@ -28,26 +27,31 @@ class Game extends React.Component{
       opp:''
     }
   }
-
-
-
-
   render() {
     let teamId = this.props.team.id;
+    let teams = this.props.allTeams;
+    let games = this.props.games;
+
     return (
       <ul className='game-grid'>
-        {this.props.games.map(function(game) {
-
+        {games.map(function(game) {
+          let opponent = teams.filter((team) => {
+            return game.opp === team.symbol
+          });
+          let opp = opponent[0].name
           let pathname = `/teams/${teamId}/games/${game.id}`
-          let date = game.date
+
+
+
 
           return (
             <li key={game.id} className='team-item'>
               <Link className='team-list' to={{pathname}} >
                 <div>{displayDate(game.date)}</div>
-                <p>{game.home === 'y' ?
-                  <span>@</span>  : <span>vs.</span>}
-                </p>
+                <span>{game.home === 'y' ?
+                  <span>@ </span>  : <span>vs. </span>}
+                </span>
+                <span>{opp}</span>
               </Link>
             </li>
           )
@@ -89,12 +93,11 @@ class ScheduleContainer extends React.Component {
       })
   }
 
-
   render() {
     return (
       <div className='schedule-container'>
         <Logo team={this.state.team} />
-        <Game team={this.state.team} games={this.state.games} />
+        <Game team={this.state.team} games={this.state.games} allTeams={this.state.allTeams} />
       </div>
     )
   }
