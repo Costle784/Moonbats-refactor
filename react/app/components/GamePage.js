8f9e298d77bb517ca550904944bc19e74e6d845e
Curtis Ostle <curtisostle@gmail.com>
2017-09-25 13:51:35 -0400

    finished selected game page except moon button

--- react/app/components/GamePage.js
+++ react/app/components/GamePage.js
@@ -1,15 +1,34 @@
 var React = require('react');
 var api = require('../utils/api')
 
-class GamePage extends React.Component {
+function GamePage(props) {
+  let team = props.selectedTeam;
+  let opp = props.opp;
+  let game = props.game
 
+  console.log(props.opp)
 
-
-  render() {
     return(
-      <div>Gamepage</div>
+      <div className='game-container'>
+        <h2 className='game-date'>{game.date}</h2>
+        <div className='selected-game-container'>
+          <div className='team-list'>
+            <img src={team.logo} alt={`logo for ${team.name}`} className='logo'/>
+            <p className='team-name'>{team.name}</p>
+          </div>
+          <p className='vs'>VS.</p>
+          <div className='team-list'>
+            <img src={opp.logo} alt={`logo for ${opp.name}`} className='logo'/>
+            <p className='team-name'>{opp.name}</p>
+          </div>
+        </div>
+        <div className='game-location'>
+          <h2>{game.location}</h2>
+        </div>
+      </div>
+
     )
   }
-}
+
 
 module.exports = GamePage;
