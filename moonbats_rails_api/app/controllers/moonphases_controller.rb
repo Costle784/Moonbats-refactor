741c7e8e770ff36ade9159b8c88a35f54328c029
Curtis Ostle <curtisostle@gmail.com>
2017-09-30 13:19:18 -0400

    making moon loading screen

--- moonbats_rails_api/app/controllers/moonphases_controller.rb
+++ moonbats_rails_api/app/controllers/moonphases_controller.rb
@@ -2,11 +2,15 @@ class MoonphasesController < ApplicationController
 
   def index
     if params[:date]
+      #finds phase where the date is the same as the selectedGame
       @moonphase = Moonphase.where("date = ?", DateTime.parse(params[:date])).first
+      #returns all phases that are the same as the one above
       @moonphases = Moonphase.where("phase = ?", @moonphase.phase)
     else
       @moonphases = Moonphase.all
     end
+
+    # either all phases that are same phase as the game phase or all phases
     render json: @moonphases
   end
 end
