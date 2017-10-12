c9bdba391bc7cc85b324b653eb000379fc9232c9
Curtis Ostle <curtisostle@gmail.com>
2017-10-11 22:11:24 -0400

    made a sticky footer

--- react/App/components/Home.js
+++ react/App/components/Home.js
@@ -4,11 +4,13 @@ const Link = require('react-router-dom').Link;
 const Home = () => {
   return (
     <div className='home-container'>
-      <img src='https://northwoodsleague.com/duluth-huskies/wp-content/themes/northwoodsleagueTeams-DuluthHuskies/img/baseball-nl-footer.png' className='home-logo' alt='baseball bats'></img>
-      <h2 className='home-description'>Pick baseball game winners based on the phases <br />of the moon!</h2>
-      <Link className='button begin-button' to='./teams'>
-        Begin
-      </Link>
+      <div className='main-home'>
+        <img src='https://northwoodsleague.com/duluth-huskies/wp-content/themes/northwoodsleagueTeams-DuluthHuskies/img/baseball-nl-footer.png' className='home-logo' alt='baseball bats'></img>
+        <h2 className='home-description'>Pick baseball game winners based on the phases <br />of the moon!</h2>
+        <Link className='button begin-button' to='./teams'>
+          Begin
+        </Link>
+      </div>
       <footer>
         <div className='warning'>** For Entertainment Purposes Only ** </div>
         <div>
